4e8e07f22c0b93bf4532357817f6beda7b2a1ca8
usu2068 <administrador@ustarizabogados.com>
2025-03-31 12:34:46 -0500

    modulo com_ajax documentado - KP

--- administrator/components/com_ajax/ajax.php
+++ administrator/components/com_ajax/ajax.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * Archivo de entrada principal para el componente AJAX en joomla
  * @package     Joomla.Administrator
  * @subpackage  com_ajax
  *
@@ -7,6 +8,8 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
+ //Restringe el acceso directo al archivo por seguridad
 defined('_JEXEC') or die;
 
+//incluye el archivo principal del componente JAX desde el sitio frontend
 require_once JPATH_SITE . '/components/com_ajax/ajax.php';
